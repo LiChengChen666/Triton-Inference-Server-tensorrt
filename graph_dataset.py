3a6fcfe3e7cae42d0df2eb2d41d782b1f705e6dd
LiChengChen666 <53205956+LiChengChen666@users.noreply.github.com>
2024-09-22 14:17:31 +0800

    Add files via upload

---
+++ graph_dataset.py
@@ -0,0 +1,366 @@
+import logging
+import functools
+import json
+import os
+from pathlib import Path
+import numpy as np
+import torch
+from dgl.geometry import farthest_point_sampler
+try:
+    import dgl
+    from dgl.data import DGLDataset
+except ImportError:
+    raise ImportError(
+        "Mesh Graph Net Datapipe requires the DGL library. Install the "
+        + "desired CUDA version at: https://www.dgl.ai/pages/start.html"
+    )
+from torch.nn import functional as F
+import scipy.sparse as sp
+
+try:
+    import pyvista as pv
+    import vtk
+    from vtkmodules.util.numpy_support import vtk_to_numpy
+except ImportError:
+    raise ImportError(
+        "DrivAerNet Dataset requires the vtk and pyvista libraries. "
+        "Install with pip install vtk pyvista"
+    )
+
+from dgl import save_graphs, load_graphs
+from dgl.data.utils import makedirs, save_info, load_info
+
+
+class GraphDataset(DGLDataset):
+    """In-memory MeshGraphNet Dataset for stationary mesh
+    Parameters
+    ----------
+    name : str, optional
+        Name of the dataset, by default "dataset"
+    data_dir : _type_, optional
+        Specifying the directory that stores the raw data in .TFRecord format., by default None
+    split : str, optional
+        Dataset split ["train", "eval", "test"], by default "train"
+    invar_keys: Iterable[str], optional
+        The input node features to consider. Default includes 'pos'.
+    outvar_keys: Iterable[str], optional
+        The output features to consider. Default includes 'p' and 'wallShearStress'.
+    normalize_keys Iterable[str], optional
+        The features to normalize. Default includes 'p' and 'wallShearStress'.
+    cache_dir: str, optional
+        Path to the cache directory to store graphs in DGL format for fast loading.
+        Default is ./cache/.
+    num_samples : int, optional
+        Number of samples, by default 1000
+    num_steps : int, optional
+        Number of time steps in each sample, by default 600
+    noise_std : float, optional
+        The standard deviation of the noise added to the "train" split, by default 0.02
+    force_reload : bool, optional
+        force reload, by default False
+    verbose : bool, optional
+        verbose, by default False
+    """
+    def __init__(
+        self,
+        name="dataset",
+        data_dir=None,
+        split="train",
+        point_data_keys = [],
+        field_data_keys = [],
+        normalize_keys = [],
+        radius=1,
+        input_window_size=1,
+        output_window_size=1,
+        noise_std=0.02,
+        force_reload=False,
+        verbose=False,
+        sampling=False
+    ):
+        super().__init__(
+            name=name,
+            force_reload=force_reload,
+            verbose=verbose,
+        )
+        self.data_dir = Path(data_dir)
+        self.split = split
+        self.noise_std = noise_std
+        self.radius = radius
+        self.input_window_size = input_window_size
+        self.output_window_size = output_window_size
+        self.point_data_keys = point_data_keys
+        self.field_data_keys = field_data_keys
+        assert self.data_dir.is_dir(), f"错误，文件夹{self.data_dir}不存在"
+
+        self.data_paths = []
+        files = os.listdir(self.data_dir)
+        for file in files:
+            data_path = os.path.join(self.data_dir, file)
+            self.data_paths.append(data_path)
+            # logging.info(f'找到文件{data_path}')
+        self.num_samples = len(self.data_paths)
+        logging.info(f"轨迹个数：{self.num_samples}")
+
+        self.readers = []
+        self.steps = []
+        for path in self.data_paths:
+            reader = vtk.vtkHDFReader()
+            reader.SetFileName(path)
+            reader.UpdateInformation()
+            self.steps.append(reader.GetNumberOfSteps())
+            self.readers.append(reader)
+
+        self.num_steps = min(self.steps)
+        assert self.num_steps != 0, f"没有数据"
+
+        self.length = self.num_samples * (self.num_steps - 1)
+
+        self.samples = [np.arange(self.num_steps) for _ in range(self.num_samples)]
+
+        init_pos = self.get_points([0],0)[0]
+        self.sampling = sampling
+        if(sampling == True):
+            mesh_size =  np.random.randint(int(0.008*init_pos.shape[0]), int(0.01*init_pos.shape[0]))
+            print('mesh_size:',mesh_size)
+            while(mesh_size %10 !=0):
+                    mesh_size += 1
+            self.point_idx = farthest_point_sampler(init_pos.unsqueeze(0), mesh_size)[0]
+
+            graph = dgl.knn_graph(init_pos[self.point_idx], 5)
+            self.graph  = dgl.to_bidirected(graph)
+        else:
+            self.graph = self._create_dgl_graph(0)
+        # print(self.graph)
+
+    def __getitem__(self, idx):
+        dp_idx = int(idx / self.num_steps)
+        time_idx = int(idx % self.num_steps)
+        in_idx = self.samples[dp_idx][time_idx]
+
+        in_idxs = []
+        for i in range(self.input_window_size):
+            in_idx_temp = in_idx + i * 1
+            if in_idx_temp > (self.num_steps - 1):
+                in_idx_temp = self.num_steps - 1
+            in_idxs.append(in_idx_temp)
+
+        out_idxs = []
+        for i in range(self.output_window_size):
+            out_idx = in_idx + (i + 1) * self.input_window_size
+            if out_idx > (self.num_steps - 1):
+                out_idx = self.num_steps - 1
+            out_idxs.append(out_idx)
+        in_pos,out_pos,in_pos_data,out_pos_data,field_data= self.get_features(in_idxs,out_idxs,dp_idx)
+        if self.sampling == True:
+            in_pos = in_pos[:,self.point_idx]
+            out_pos = out_pos[:,self.point_idx]
+            in_pos_data = in_pos_data[:,self.point_idx]
+            out_pos_data = out_pos_data[:,self.point_idx]
+
+        field_data = field_data.flatten().unsqueeze(0).repeat(in_pos_data.size(1),1)
+
+        dim = in_pos[-1].size(-1)
+        # print(self.graph.edges()[0])
+        edge_displacement = (torch.gather(in_pos[-1], dim=0, index=self.graph.edges()[0].unsqueeze(-1).expand(-1, dim)) -
+                   torch.gather(in_pos[-1], dim=0, index=self.graph.edges()[1].unsqueeze(-1).expand(-1, dim)))
+        if(self.radius is not None):
+            edge_displacement /= self.radius
+        edge_distance = torch.norm(edge_displacement, dim=-1, keepdim=True)
+        # print(edge_displacement)
+        # print(in_pos_data.shape,in_pos.shape,field_data.shape)
+        in_pos = in_pos.permute(1,0,2).reshape(in_pos_data.size(1), -1)
+
+        node_features = torch.cat(  
+            (in_pos_data.permute(1,0,2).reshape(in_pos_data.size(1), -1), in_pos, field_data), dim=-1
+        )
+        node_targets = torch.cat(
+            (out_pos_data.permute(1,0,2).reshape(in_pos_data.size(1), -1), out_pos.permute(1,0,2).reshape(in_pos_data.size(1), -1)), dim=-1
+        )
+        edge_features = torch.cat((edge_displacement, edge_distance), dim=-1)
+        # print(edge_features,edge_features.shape)
+        
+        self.graph.ndata["pos"] = in_pos
+        self.graph.ndata["x"] = node_features
+        self.graph.ndata["y"] = node_targets
+        self.graph.edata['x'] = edge_features
+
+        return self.graph
+    
+    def __len__(self):
+        # 数据样本的数量
+        return self.length
+
+    def process(self):
+        # 将原始数据处理为图、标签和数据集划分的掩码
+
+        pass
+    def save(self):
+        # 将处理后的数据保存至 `self.save_path`
+        pass
+
+    def load(self):
+        # 从 `self.save_path` 导入处理后的数据
+        pass
+
+    def get_features(self,input_idxs,output_idxs,dp_idx):
+        idxs = input_idxs+output_idxs
+        in_ends = len(input_idxs)
+        pos = []
+        pd_data = []
+        fd_data = []
+        for idx in idxs:
+            reader = self.readers[dp_idx]
+            reader.SetStep(idx)
+            reader.Update()
+            output = reader.GetOutput()
+            field_data = output.GetFieldData()
+            f = []
+            for name in self.field_data_keys:
+                data = vtk_to_numpy(field_data.GetArray(name))
+                f.append(torch.tensor(data, dtype=torch.float))
+            f = torch.cat(f, dim=-1)[None, ...]
+            fd_data.append(f)
+            points = torch.tensor(vtk_to_numpy(output.GetPoints().GetData())[None,...])
+            pos.append(points)
+            pd = []
+            for key in self.point_data_keys:
+                point_data = vtk_to_numpy(output.GetPointData().GetArray(key))
+                if point_data.ndim == 1:
+                    point_data = np.expand_dims(point_data, axis=-1)
+                point_data = np.nan_to_num(point_data, nan=0)
+                pd.append(torch.tensor(point_data))
+            pd = torch.cat(pd, dim=-1)[None, ...]
+            pd_data.append(pd)
+        pos = torch.cat(pos, dim=0)
+        pd_data = torch.cat(pd_data, dim=0)
+        fd_data = torch.cat(fd_data, dim=0)
+        # print(pos.shape,pd_data.shape,fd_data.shape)
+        # print(input_idxs,output_idxs)
+        in_pos = pos[:in_ends]
+        out_pos = pos[in_ends:]
+        in_pos_data = pd_data[:in_ends]
+        out_pos_data = pd_data[in_ends:]
+        in_field_data = fd_data[:in_ends]
+        return in_pos,out_pos,in_pos_data,out_pos_data,in_field_data
+    
+    def get_points(self, idxs, dp_idx):
+        xx = []
+        for idx in idxs:
+            reader = self.readers[dp_idx]
+            reader.SetStep(idx)
+            reader.Update()
+            output = reader.GetOutput()
+            points = vtk_to_numpy(output.GetPoints().GetData())[None,...]
+            xx.append(torch.tensor(points))
+        xx = torch.cat(xx,dim=0)
+        return xx
+
+    @staticmethod
+    def add_edge_features(graph, pos):
+        """
+        adds relative displacement & displacement norm as edge features
+        """
+        row, col = graph.edges()
+        disp = torch.tensor(pos[row.long()] - pos[col.long()])
+        disp_norm = torch.linalg.norm(disp, dim=-1, keepdim=True)
+        graph.edata["x"] = torch.cat((disp, disp_norm), dim=1)
+        return graph
+
+    @staticmethod
+    def normalize_node(invar, mu, std):
+        """normalizes a tensor"""
+        if (invar.size()[-1] != mu.size()[-1]) or (invar.size()[-1] != std.size()[-1]):
+            raise AssertionError("input and stats must have the same size")
+        return (invar - mu.expand(invar.size())) / std.expand(invar.size())
+
+    @staticmethod
+    def normalize_edge(graph, mu, std):
+        """normalizes a tensor"""
+        if (
+            graph.edata["x"].size()[-1] != mu.size()[-1]
+            or graph.edata["x"].size()[-1] != std.size()[-1]
+        ):
+            raise AssertionError("Graph edge data must be same size as stats.")
+        return (graph.edata["x"] - mu) / std
+
+    @staticmethod
+    def denormalize(invar, mu, std):
+        """denormalizes a tensor"""
+        denormalized_invar = invar * std + mu
+        return denormalized_invar
+    
+    @staticmethod
+    def _push_forward_diff(invar):
+        return torch.tensor(invar[1:] - invar[0:-1], dtype=torch.float)
+
+    @staticmethod
+    def _add_noise(features, targets, noise_std, noise_mask):
+        noise = torch.normal(mean=0, std=noise_std, size=features.size())
+        noise_mask = noise_mask.expand(features.size()[0], -1, 2)
+        noise = torch.where(noise_mask, noise, torch.zeros_like(noise))
+        features += noise
+        targets -= noise
+        return features, targets
+    
+    def _create_dgl_graph(
+        self,
+        sample_id: int,
+        to_bidirected: bool = True,
+        dtype: torch.dtype | str = torch.int64,
+    ) -> dgl.DGLGraph:
+        
+        reader = self.readers[sample_id]
+        reader.SetStep(0)
+        reader.Update()
+        grid = reader.GetOutput()
+        cells = grid.GetCells().GetData()
+        points = vtk_to_numpy(grid.GetPoints().GetData())
+        cell_indices = vtk_to_numpy(cells)
+        edge = set()
+        i = 0
+        while i < len(cell_indices):
+            num_points = cell_indices[i]
+            indices = cell_indices[i+1:i+1+num_points]
+            for j in range(num_points):
+                for k in range(j+1,num_points):
+                    edge.add((indices[j],indices[k]))
+                    edge.add((indices[k],indices[j]))
+            i += (num_points + 1)
+        
+        row, col = zip(*edge)
+        data = np.ones(len(row))
+        adj_matrix = sp.coo_matrix((data,(row,col)),shape=(points.shape[0],points.shape[0]))
+        graph = dgl.from_scipy(adj_matrix,idtype=dtype) 
+        if to_bidirected:
+            graph = dgl.to_bidirected(graph)
+        return graph
+    
+if __name__ == '__main__':
+    import h5py as h5
+    from trans import UnitGaussianNormalizer
+    dataset = GraphDataset(data_dir='/home/h5',
+                             point_data_keys=['Effective Stress','Temperature'],
+                             field_data_keys=['Time','a'])
+    with h5.File('/home/h5/out1.hdf', 'r') as f:
+        Temperature = f['VTKHDF/PointData/Temperature'][:]
+        Stress = f['VTKHDF/PointData/Effective Stress'][:]
+
+    print(Temperature.shape)
+        
+    normalizers = UnitGaussianNormalizer.from_dataset(
+                    [{'Temperature':torch.tensor(Temperature),'Stress':torch.tensor(Stress)}], dim=[1], keys=['Temperature','Stress']
+                )
+
+    print(normalizers['Temperature'].mean,normalizers['Temperature'].std)
+    print(normalizers['Stress'].mean,normalizers['Stress'].std)
+
+    import time 
+    t1 = time.time()
+    # a = dataset[10].edge_index - dataset[0].edge_index
+    # is_all_zero = torch.sum(a)
+    print(dataset[0].ndata['x'].shape)
+    # for i in range(10):
+    #     dataset[i]
+    # print(is_all_zero)
+    t2 = time.time()
+    print((t2-t1)/10)
---
+++ train2.py
@@ -0,0 +1,198 @@
+
+import time
+
+import hydra
+from hydra.utils import to_absolute_path
+import torch
+import wandb
+
+from dgl.dataloading import GraphDataLoader
+
+from omegaconf import DictConfig
+
+from torch.cuda.amp import GradScaler, autocast
+from torch.nn.parallel import DistributedDataParallel
+
+# from modulus.datapipes.gnn.vortex_shedding_dataset import VortexSheddingDataset
+from data.graph_dataset import GraphDataset
+from modulus.distributed.manager import DistributedManager
+from modulus.launch.logging import (
+    PythonLogger,
+    RankZeroLoggingWrapper,
+    initialize_wandb,
+)
+from modulus.launch.utils import load_checkpoint, save_checkpoint
+from modulus.models.meshgraphnet import MeshGraphNet
+from utils import LpLoss
+
+class MGNTrainer:
+    def __init__(self, cfg: DictConfig, rank_zero_logger: RankZeroLoggingWrapper):
+        assert DistributedManager.is_initialized()
+        self.dist = DistributedManager()
+
+        self.amp = cfg.amp
+        # MGN with recompute_activation currently supports only SiLU activation function.
+        mlp_act = "relu"
+        if cfg.recompute_activation:
+            rank_zero_logger.info(
+                "Setting MLP activation to SiLU required by recompute_activation."
+            )
+            mlp_act = "silu"
+
+        # instantiate dataset
+        dataset = GraphDataset(data_dir=to_absolute_path(cfg.data_dir),
+            point_data_keys=['Temperature'],
+            field_data_keys=['Time'])
+
+        # instantiate dataloader
+        self.dataloader = GraphDataLoader(
+            dataset,
+            batch_size=cfg.batch_size,
+            shuffle=True,
+            drop_last=True,
+            pin_memory=True,
+            use_ddp=self.dist.world_size > 1,
+            num_workers=cfg.num_dataloader_workers,
+        )
+
+        # instantiate the model
+        self.model = MeshGraphNet(
+            cfg.num_input_features,
+            cfg.num_edge_features,
+            cfg.num_output_features,
+            mlp_activation_fn=mlp_act,
+            do_concat_trick=cfg.do_concat_trick,
+            num_processor_checkpoint_segments=cfg.num_processor_checkpoint_segments,
+            recompute_activation=cfg.recompute_activation,
+        )
+        if cfg.jit:
+            if not self.model.meta.jit:
+                raise ValueError("MeshGraphNet is not yet JIT-compatible.")
+            self.model = torch.jit.script(self.model).to(self.dist.device)
+        else:
+            self.model = self.model.to(self.dist.device)
+        if cfg.watch_model and not cfg.jit and self.dist.rank == 0:
+            wandb.watch(self.model)
+
+        # distributed data parallel for multi-node training
+        if self.dist.world_size > 1:
+            self.model = DistributedDataParallel(
+                self.model,
+                device_ids=[self.dist.local_rank],
+                output_device=self.dist.device,
+                broadcast_buffers=self.dist.broadcast_buffers,
+                find_unused_parameters=self.dist.find_unused_parameters,
+            )
+
+        # enable train mode
+        self.model.train()
+
+        # instantiate loss, optimizer, and scheduler
+        self.criterion = LpLoss(d=2,p=2)
+
+        self.optimizer = None
+        try:
+            if cfg.use_apex:
+                from apex.optimizers import FusedAdam
+
+                self.optimizer = FusedAdam(self.model.parameters(), lr=cfg.lr)
+        except ImportError:
+            rank_zero_logger.warning(
+                "NVIDIA Apex (https://github.com/nvidia/apex) is not installed, "
+                "FusedAdam optimizer will not be used."
+            )
+        if self.optimizer is None:
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=cfg.lr)
+        rank_zero_logger.info(f"Using {self.optimizer.__class__.__name__} optimizer")
+
+        self.scheduler = torch.optim.lr_scheduler.LambdaLR(
+            self.optimizer, lr_lambda=lambda epoch: cfg.lr_decay_rate**epoch
+        )
+        self.scaler = GradScaler()
+
+        # load checkpoint
+        if self.dist.world_size > 1:
+            torch.distributed.barrier()
+        self.epoch_init = load_checkpoint(
+            to_absolute_path(cfg.ckpt_path),
+            models=self.model,
+            optimizer=self.optimizer,
+            scheduler=self.scheduler,
+            scaler=self.scaler,
+            device=self.dist.device,
+        )
+
+    def train(self, graph):
+        graph = graph.to(self.dist.device)
+        self.optimizer.zero_grad()
+        loss = self.forward(graph)
+        self.backward(loss)
+        self.scheduler.step()
+        return loss
+
+    def forward(self, graph):
+        # forward pass
+        with autocast(enabled=self.amp):
+            pred = self.model(graph.ndata["x"], graph.edata["x"], graph)
+            loss = self.criterion(pred, graph.ndata["y"])
+            return loss
+
+    def backward(self, loss):
+        # backward pass
+        if self.amp:
+            self.scaler.scale(loss).backward()
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+        else:
+            loss.backward()
+            self.optimizer.step()
+
+
+@hydra.main(version_base="1.3", config_path="config", config_name="config")
+def main(cfg: DictConfig) -> None:
+    # initialize distributed manager
+    DistributedManager.initialize()
+    dist = DistributedManager()
+
+    # Initialize loggers.
+    initialize_wandb(
+        project="Modulus-Launch",
+        entity="Modulus",
+        name="Vortex_Shedding-Training",
+        group="Vortex_Shedding-DDP-Group",
+        mode=cfg.wandb_mode,
+    )  # Wandb logger
+    logger = PythonLogger("main")  # General python logger
+    rank_zero_logger = RankZeroLoggingWrapper(logger, dist)  # Rank 0 logger
+    rank_zero_logger.file_logging()
+
+    trainer = MGNTrainer(cfg, rank_zero_logger)
+    start = time.time()
+    rank_zero_logger.info("Training started...")
+    for epoch in range(trainer.epoch_init, cfg.epochs):
+        for graph in trainer.dataloader:
+            loss = trainer.train(graph)
+        rank_zero_logger.info(
+            f"epoch: {epoch}, loss: {loss:10.3e}, time per epoch: {(time.time()-start):10.3e}"
+        )
+        wandb.log({"loss": loss.detach().cpu()})
+
+        # save checkpoint
+        # if dist.world_size > 1:
+        #     torch.distributed.barrier()
+        # if dist.rank == 0:
+        #     save_checkpoint(
+        #         to_absolute_path(cfg.ckpt_path),
+        #         models=trainer.model,
+        #         optimizer=trainer.optimizer,
+        #         scheduler=trainer.scheduler,
+        #         scaler=trainer.scaler,
+        #         epoch=epoch,
+        #     )
+        #     logger.info(f"Saved model on rank {dist.rank}")
+        start = time.time()
+    rank_zero_logger.info("Training completed!")
+
+
+if __name__ == "__main__":
+    main()
